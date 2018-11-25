a790341addda9e65ad9cd6bb79a7b80b9cba7fd2
George Cave <g.cave@kiska.com>
2018-11-25 19:23:43 +0000

    Create first working proto of picture -> sftp upload

---
+++ camera.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import gmtime,strftime
+from paramiko import SSHClient, AutoAddPolicy
+import sftp as creds
+
+
+global img_filename
+
+def button1_callback(channel):
+	print("CAMERA BUTTON!")
+	take_picture()
+	print("FINISHED!")
+
+def button2_callback(channel):
+	print("BUTTON 2")
+
+def take_picture():
+	img_filename = strftime("cam-%Y-%m-%d_%H:%M:%S.png", gmtime())
+	camera.capture(img_filename)
+
+	print("PICTURE CAPTURED!")
+	
+	with SSHClient() as client:
+		client.set_missing_host_key_policy(AutoAddPolicy())
+		client.connect(
+			creds.hostname,
+			port=22,
+			username=creds.username,
+			password=creds.password
+		)
+		with client.open_sftp() as sftp:
+			sftp.put(img_filename,creds.path + '/' + img_filename)
+
+
+
+camera = PiCamera()
+camera.resolution = (800,600)
+
+button1 = 2
+button2 = 3
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+
+
+
+GPIO.add_event_detect(button1,GPIO.FALLING,callback=button1_callback)
+GPIO.add_event_detect(button2,GPIO.FALLING,callback=button2_callback)
+
+message=input("Press enter to quit \n\n")
+GPIO.cleanup()
---
+++ sftp-template.py
@@ -0,0 +1,10 @@
+
+# 1) Fill out variables below for SFTP connection details
+# 2) Rename this file to sftp.py
+
+hostname = "SFTP_HOSTNAME"
+username = "SFTP_USERNAME"
+password = "SFTP_PASSWORD"
+
+# No trailing slash here please :)
+path = "PATH_ON_sERVER_TO_PLACE_IMAGES"
