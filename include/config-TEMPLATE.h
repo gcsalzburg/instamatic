923431927464fbd6dfd1121698d7082f6f1eeaac
George Cave <32631912+gcsalzburg@users.noreply.github.com>
2019-11-17 17:37:48 +0100

    Fix issues so can compile on other PCs

---
+++ include/config-TEMPLATE.h
@@ -0,0 +1,9 @@
+
+// Your WiFi credentials
+char ssid[] = "WIFI_NETWORK_NAME";
+char pass[] = "WIFI_PASSWORD";
+
+// FTP Server credentials
+char ftp_server[] = "server.com";
+char ftp_user[] = "my_ftp_username";
+char ftp_pass[] = "my_ftp_password";
